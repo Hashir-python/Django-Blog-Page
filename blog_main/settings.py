3449ad5a3e55890617a5b54dd3165a5b2f0672f8
Hashir-python <hashirxr452@gmail.com>
2026-01-03 18:26:48 +0500

    blog app and blog model

--- blog_main/settings.py
+++ blog_main/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'blogs',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -119,3 +120,6 @@ STATIC_ROOT=BASE_DIR /'static'
 STATICFILES_DIRS=[
     'blog_main/static',
 ]
+
+MEDIA_URL='/media/'
+MEDIA_ROOT=BASE_DIR /'media'
