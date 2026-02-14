adc18163b4a9f1cbfa301ccc9614c00ac53fbcb5
Hashir-python <hashirxr452@gmail.com>
2026-02-15 00:52:51 +0500

    Category crud functionallity

--- blog_main/settings.py
+++ blog_main/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'blogs',
     'assignments',
+    'dashboard',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- blog_main/urls.py
+++ blog_main/urls.py
@@ -27,8 +27,11 @@ urlpatterns = [
     path('',views.home,name='home'),
     path('category/',include('blogs.urls')),
     path('blogs/<slug:slug>/',BlogsViews.blogs,name='blogs'),
+    #search endpoints
     path('blogs/search/',BlogsViews.search,name='search'),
     path('register/',views.register,name='register'),
     path('login/',views.login,name='login'),
-    path('logout/',views.logout,name='logout')
+    path('logout/',views.logout,name='logout'),
+    #dashboards
+    path('dashboard/',include('dashboard.urls'),name='login')
 ]+static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
